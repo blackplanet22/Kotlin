56cc0b117ce8e2cb5ee06dc76ea24462651d4582
Marin <blackplanet22@gmail.com>
2023-03-01 14:46:24 +0100

    Wuhu

---
+++ app/src/main/java/com/example/orderlist/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.orderlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.orderlist.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var todoAdapter: TodoAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        todoAdapter = TodoAdapter(mutableListOf())
+
+        binding.rvTodoItems.adapter = todoAdapter
+        binding.rvTodoItems.layoutManager = LinearLayoutManager(this)
+
+        binding.btnAddTodo.setOnClickListener {
+            val todoTitle = binding.etTodoTitle.text.toString()
+            if (todoTitle.isNotEmpty()) {
+                val todo = Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                binding.etTodoTitle.text.clear()
+            }
+        }
+        binding.btnDeleteTodos.setOnClickListener {
+            todoAdapter.deleteDoneTodos()
+        }
+    }
+}
